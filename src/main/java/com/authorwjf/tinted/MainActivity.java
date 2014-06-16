968a57fce9898521aea398f209c8860d60d243c3
John Lombardo <john@lombardos.org>
2014-06-16 16:59:20 -0700

    Added code from another demo.

--- src/main/java/com/authorwjf/tinted/MainActivity.java
+++ src/main/java/com/authorwjf/tinted/MainActivity.java
@@ -1,5 +1,6 @@
 package com.authorwjf.tinted;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -40,4 +41,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		return getResources().getIdentifier(name,"id",getPackageName());
 	}
 
+    public void startAnother1(View view) {
+        startActivity(new Intent(this,Another1.class));
+    }
 }
