55487af65d55e885d73b4d1c9330a0d5e7e37354
John Lombardo <john@lombardos.org>
2014-06-16 16:02:58 -0700

    Initial commit of William J. Francis' demo

---
+++ src/main/java/com/authorwjf/tinted/MainActivity.java
@@ -0,0 +1,43 @@
+package com.authorwjf.tinted;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.PorterDuff.Mode;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private String[] colors = {"white", "magenta", "yellow", "cyan", "green"};
+	private int index = 0;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		for (int i=0; i<5; i++) {
+			String name = "button_"+Integer.toString(i+1);
+			findViewById(getIdFromName(name)).setOnClickListener(this);
+		}
+		onClick(null);
+	}
+
+	@Override
+	public void onClick(View v) {
+		for (int i=0; i<5; i++) {
+			String name = "button_"+Integer.toString(i+1);
+			((Button)findViewById(getIdFromName(name))).setText(colors[index]);
+			int tint = Color.parseColor(colors[index]); 
+			((Button)findViewById(getIdFromName(name))).getBackground().setColorFilter(tint, Mode.MULTIPLY);
+		}
+		index++;
+		if (index>4) index=0;
+	}
+	
+	private int getIdFromName(String name) {
+		return getResources().getIdentifier(name,"id",getPackageName());
+	}
+
+}
